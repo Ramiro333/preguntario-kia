ad4bb624b135bc1760dd98732f0b9a23bdb9b0d8
ramiro reyna <ramirololreyna@gmail.com>
2024-06-03 21:20:25 -0300

    mejorado la forma de elegir un conjunto prestablecido, puesto indice en conjunto de preguntas

--- js/script.js
+++ js/script.js
@@ -12,10 +12,11 @@ class conjuntoPreguntas {
         return this.preguntas[GETRANDOM];
     }
     crearDiv(contenidoPregunta){
+        //validar preguntas repetidas
         if(contenidoPregunta == "" || contenidoPregunta== " "){
         } else {
             const DIV = document.createElement("div");
-            DIV.innerText = contenidoPregunta;
+            DIV.innerText =this.preguntas.indexOf(contenidoPregunta)+1 + " " + contenidoPregunta;
             DIV.classList.add("nuevos")
             segundo.appendChild(DIV);
         }
@@ -31,13 +32,13 @@ class conjuntoPreguntas {
     }
 }
 let conjuntoSeleccionado = new conjuntoPreguntas("",[]);
-    let preguntasdmin = ["Cuales son los roles interpersonales","Nombrar dos ejemplos de roles decisionales","Cuales son los tipos de habilides","Cuales son los niveles gerenciales organizacionales","Nombrar a quienes se los denomina gerentes de nivel institucional","Nombrar las variantes q estudia el entorno o ambiente global","Nombrar las formas de internacionalizar a una organización","Habilidad q predomina en el nivel operativo :","Competencias del administrador","Proceso del administrador defini cada uno","Nombra q hacen los diferentes generentes de los niveles organizacionales","Como se evalua el desempeño de un administrador","Características de una organizacion","Diferencias de objetivos organizacionales y objetivos individuales","Quales son los paremetros del sistema nombralos y definilos","Que es homeostasis","Tipos de retrolimentacion","Cuantas propiedades del sistema hay definilas","Como se clasifican los sistemas segun su constitucion y segun su naturaleza","Clasifica la org segun su finalidad, tamaño, regimen juridico, actividad economica","Como se clasifica la org por los sectores de actividad nombrarlos y definirlos","El ambiente general o contexto mediato impacta en la org de manera directa?(V/F)","Cuales son las variables q componen el entorno mediato o contexto general, ambiente de tarea y el ambiente interno","Aspectos formales e informales q son","Cuales son los aspectos de la cultura","Variables q se analizan dentro del riesgo politico","Cuales son las medidas politicas","Que significa outsourcing",]
+const PREGUNTASADMIN= new conjuntoPreguntas ("preguntasAdmin",["Cuales son los roles interpersonales","Nombrar dos ejemplos de roles decisionales","Cuales son los tipos de habilides","Cuales son los niveles gerenciales organizacionales","Nombrar a quienes se los denomina gerentes de nivel institucional","Nombrar las variantes q estudia el entorno o ambiente global","Nombrar las formas de internacionalizar a una organización","Habilidad q predomina en el nivel operativo :","Competencias del administrador","Proceso del administrador defini cada uno","Nombra q hacen los diferentes generentes de los niveles organizacionales","Como se evalua el desempeño de un administrador","Características de una organizacion","Diferencias de objetivos organizacionales y objetivos individuales","Quales son los paremetros del sistema nombralos y definilos","Que es homeostasis","Tipos de retrolimentacion","Cuantas propiedades del sistema hay definilas","Como se clasifican los sistemas segun su constitucion y segun su naturaleza","Clasifica la org segun su finalidad, tamaño, regimen juridico, actividad economica","Como se clasifica la org por los sectores de actividad nombrarlos y definirlos","El ambiente general o contexto mediato impacta en la org de manera directa?(V/F)","Cuales son las variables q componen el entorno mediato o contexto general, ambiente de tarea y el ambiente interno","Aspectos formales e informales q son","Cuales son los aspectos de la cultura","Variables q se analizan dentro del riesgo politico","Cuales son las medidas politicas","Que significa outsourcing",]);
 botonUsar.onclick = function(){
-    for(i=0;i<preguntasdmin.length;i++){
-        conjuntoSeleccionado.preguntas.push(preguntasdmin[i])
-        conjuntoSeleccionado.crearDiv(conjuntoSeleccionado.preguntas[i])
+    for(i=0;i<PREGUNTASADMIN.preguntas.length;i++){
+        conjuntoSeleccionado.agregarPregunta(PREGUNTASADMIN.preguntas[i]);
+        conjuntoSeleccionado.crearDiv(PREGUNTASADMIN.preguntas[i]);
+        
     }
-    console.log(conjuntoSeleccionado.preguntas.length)
     
 }
 // botonUsar.onclick = function(){
