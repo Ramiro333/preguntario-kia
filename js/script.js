8f0821c9cd824c31d07c854bc3229fb9985f0e74
Ramiro333 <ramirololreyna@gmail.com>
2024-06-18 21:29:43 -0300

    agreagado boton de eliminar todas las preguntas

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const botonUsar = document.querySelector(".boton-usar");
 const segundo = document.querySelector(".segundo");
 const contenedorDePreguntas = document.querySelector(".contenedorDePreguntas");
 const botonGuardar = document.querySelector(".boton-guardar");
+const botonEliminarTodo = document.querySelector(".boton-eliminar-todo")
 class conjuntoPreguntas {
     constructor(nombre,preguntas){
         this.nombre = nombre;
@@ -46,7 +47,7 @@ class conjuntoPreguntas {
     }
     agregarPregunta(nuevaPregunta){
         if (this.preguntasRepetidas(nuevaPregunta) || nuevaPregunta == "" || nuevaPregunta== " "){
-            console.log("pregunta repetida")
+            console.log("pregunta repetida")//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         }  else {
             this.preguntas.push(nuevaPregunta);
         }
@@ -62,6 +63,9 @@ class conjuntoPreguntas {
     agregarALocarStorage(){
         localStorage.setItem("preguntasGuardadas",JSON.stringify(conjuntoSeleccionado.preguntas))
     }
+    eliminarTodasLasPreguntas(){
+        this.preguntas = [];
+    }
 }
 // cargo preguntas de la ultima sesion//
 let conjuntoSeleccionado = new conjuntoPreguntas("",[]);
@@ -70,10 +74,10 @@ function cargarPreguntasGuardadas(){
     conjuntoSeleccionado.crearDiv()
 }
 cargarPreguntasGuardadas();
-const PREGUNTASADMIN = new conjuntoPreguntas ("preguntasAdmin",["Cuales son los roles interpersonales","Nombrar dos ejemplos de roles decisionales","Cuales son los tipos de habilides","Cuales son los niveles gerenciales organizacionales","Nombrar a quienes se los denomina gerentes de nivel institucional","Nombrar las variantes q estudia el entorno o ambiente global","Nombrar las formas de internacionalizar a una organización","Habilidad q predomina en el nivel operativo :","Competencias del administrador","Proceso del administrador defini cada uno","Nombra q hacen los diferentes generentes de los niveles organizacionales","Como se evalua el desempeño de un administrador","Características de una organizacion","Diferencias de objetivos organizacionales y objetivos individuales","Quales son los paremetros del sistema nombralos y definilos","Que es homeostasis","Tipos de retrolimentacion","Cuantas propiedades del sistema hay definilas","Como se clasifican los sistemas segun su constitucion y segun su naturaleza","Clasifica la org segun su finalidad, tamaño, regimen juridico, actividad economica","Como se clasifica la org por los sectores de actividad nombrarlos y definirlos","El ambiente general o contexto mediato impacta en la org de manera directa?(V/F)","Cuales son las variables q componen el entorno mediato o contexto general, ambiente de tarea y el ambiente interno","Aspectos formales e informales q son","Cuales son los aspectos de la cultura","Variables q se analizan dentro del riesgo politico","Cuales son las medidas politicas","Que significa outsourcing",]);
+const preguntasAdmin = new conjuntoPreguntas ("preguntasAdmin",["Cuales son los roles interpersonales","Nombrar dos ejemplos de roles decisionales","Cuales son los tipos de habilides","Cuales son los niveles gerenciales organizacionales","Nombrar a quienes se los denomina gerentes de nivel institucional","Nombrar las variantes q estudia el entorno o ambiente global","Nombrar las formas de internacionalizar a una organización","Habilidad q predomina en el nivel operativo :","Competencias del administrador","Proceso del administrador defini cada uno","Nombra q hacen los diferentes generentes de los niveles organizacionales","Como se evalua el desempeño de un administrador","Características de una organizacion","Diferencias de objetivos organizacionales y objetivos individuales","Quales son los paremetros del sistema nombralos y definilos","Que es homeostasis","Tipos de retrolimentacion","Cuantas propiedades del sistema hay definilas","Como se clasifican los sistemas segun su constitucion y segun su naturaleza","Clasifica la org segun su finalidad, tamaño, regimen juridico, actividad economica","Como se clasifica la org por los sectores de actividad nombrarlos y definirlos","El ambiente general o contexto mediato impacta en la org de manera directa?(V/F)","Cuales son las variables q componen el entorno mediato o contexto general, ambiente de tarea y el ambiente interno","Aspectos formales e informales q son","Cuales son los aspectos de la cultura","Variables q se analizan dentro del riesgo politico","Cuales son las medidas politicas","Que significa outsourcing",]);
 botonUsar.onclick = function(){
-    for(i=0;i<PREGUNTASADMIN.preguntas.length;i++){
-        conjuntoSeleccionado.agregarPregunta(PREGUNTASADMIN.preguntas[i]);
+    for(i=0;i<preguntasAdmin.preguntas.length;i++){
+        conjuntoSeleccionado.agregarPregunta(preguntasAdmin.preguntas[i]);
     }   
     conjuntoSeleccionado.crearDiv();
     
@@ -96,7 +100,10 @@ SUBMIT.onclick = function() {
 botonGuardar.addEventListener("click",()=>{
     conjuntoSeleccionado.agregarALocarStorage()
 })
-
+botonEliminarTodo.addEventListener("click",()=>{
+    contenedorDePreguntas.innerHTML="";
+    conjuntoSeleccionado.eliminarTodasLasPreguntas();
+})
 
 
 
