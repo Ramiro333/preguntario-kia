35307ceb7233d105731048b7c9605dc9a94844e9
Tu Nombre <tuemail@example.com>
2024-06-30 23:51:05 +0200

    creado funcion para mostrar mensajes en el dom, reestructurado el conjunto predeterminado -preguntasadmin-

--- js/script.js
+++ js/script.js
@@ -28,7 +28,16 @@ const botonGuardarConjuntos = document.querySelector(".boton-guardar-conjuntos")
 const botonEliminarConjuntosGuardados = document.querySelector(".boton-eliminar-conjuntos-guardados");
 const contenedorGuardarConjuntos = document.querySelector(".contenedor-guardar-conjuntos");
 const mensajeConjuntosGuardados = document.createElement("p");
-
+function nuevoerror(textoDelEeror,lugarDelError,timeout){
+    nuevoP = document.createElement("p");
+    nuevoP.innerText=textoDelEeror;
+    nodoDestino = lugarDelError;
+    nodoDestino.appendChild(nuevoP);
+    if(timeout != undefined){
+        setTimeout(()=> nuevoP.remove(),timeout);
+    }
+    
+}
 let todosLosConjuntos = [];
 class conjuntoPreguntas {
     constructor(nombre,preguntas,lugar){
@@ -77,10 +86,11 @@ class conjuntoPreguntas {
         if (this.preguntasRepetidas(nuevaPregunta)){
 
         } else if(nuevaPregunta == "" || nuevaPregunta== " "){
-            const errorPreguntaRepetida = document.createElement("p");
-            errorPreguntaRepetida.innerHTML="la pregunta no puede estar vacia";
-            this.lugar.parentNode.appendChild(errorPreguntaRepetida);
-            setTimeout(()=> errorPreguntaRepetida.remove(),3000);
+            nuevoerror("la pregunta no puede estar vacia",this.lugar.parentNode,3000)
+            // const errorPreguntaRepetida = document.createElement("p");
+            // errorPreguntaRepetida.innerHTML="la pregunta no puede estar vacia";
+            // this.lugar.parentNode.appendChild(errorPreguntaRepetida);
+            // setTimeout(()=> errorPreguntaRepetida.remove(),3000);
         }
         else {
             this.preguntas.push(nuevaPregunta);
@@ -118,31 +128,28 @@ botonCargar.addEventListener("click",()=>{
     if(localStorage.preguntasGuardadas.length>2){
         cargarPreguntasGuardadas()
     } else {
-        errorCargar.innerText="no hay preguntas guardadas";
-        contenedorCargarGuardar.appendChild(errorCargar);
-        setTimeout(()=> errorCargar.remove(),3000);
+        nuevoerror("no hay preguntas guardadas",contenedorCargarGuardar, 3000)
+        // errorCargar.innerText="no hay preguntas guardadas";
+        // contenedorCargarGuardar.appendChild(errorCargar);
+        // setTimeout(()=> errorCargar.remove(),3000);
     }
 })
 
-const preguntasAdmin = new conjuntoPreguntas ("preguntasAdmin",["Cuales son los roles interpersonales","Nombrar dos ejemplos de roles decisionales","Cuales son los tipos de habilides","Cuales son los niveles gerenciales organizacionales","Nombrar a quienes se los denomina gerentes de nivel institucional","Nombrar las variantes q estudia el entorno o ambiente global","Nombrar las formas de internacionalizar a una organización","Habilidad q predomina en el nivel operativo :","Competencias del administrador","Proceso del administrador defini cada uno","Nombra q hacen los diferentes generentes de los niveles organizacionales","Como se evalua el desempeño de un administrador","Características de una organizacion","Diferencias de objetivos organizacionales y objetivos individuales","Quales son los paremetros del sistema nombralos y definilos","Que es homeostasis","Tipos de retrolimentacion","Cuantas propiedades del sistema hay definilas","Como se clasifican los sistemas segun su constitucion y segun su naturaleza","Clasifica la org segun su finalidad, tamaño, regimen juridico, actividad economica","Como se clasifica la org por los sectores de actividad nombrarlos y definirlos","El ambiente general o contexto mediato impacta en la org de manera directa?(V/F)","Cuales son las variables q componen el entorno mediato o contexto general, ambiente de tarea y el ambiente interno","Aspectos formales e informales q son","Cuales son los aspectos de la cultura","Variables q se analizan dentro del riesgo politico","Cuales son las medidas politicas","Que significa outsourcing"],primer);
-//funcion solo a usar para meter preguntas a las seleccionadas
 function agregarPreguntasAlConjunto(arrayAgregar){
     for(i=0;i<arrayAgregar.preguntas.length;i++){
         conjuntoSeleccionado.agregarPregunta(arrayAgregar.preguntas[i]);
     }
     conjuntoSeleccionado.crearDiv(primer);
 }
-botonUsar.onclick = function(){
-    agregarPreguntasAlConjunto(preguntasAdmin);
 
-}
 const errorPocasPreguntas = document.createElement("p");
 botonRandom.onclick = function(){
     //primero valido que haya suficientes preguntas
     if (conjuntoSeleccionado.preguntas.length < 2 ){
-        errorPocasPreguntas.innerHTML="debes elegir al menos 2 preguntas o utilizar un conjunto";
-        preguntero.appendChild(errorPocasPreguntas);
-        setTimeout(()=> errorPocasPreguntas.remove(),6000);
+        nuevoerror("debes elegir al menos 2 preguntas o utilizar un conjunto",preguntero,6000)
+        // errorPocasPreguntas.innerHTML="debes elegir al menos 2 preguntas o utilizar un conjunto";
+        // preguntero.appendChild(errorPocasPreguntas);
+        // setTimeout(()=> errorPocasPreguntas.remove(),6000);
         containerPreguntas.innerText = "";
     }
         else {
@@ -159,8 +166,9 @@ submit.onclick = function() {
 //guardo las preguntas en local storage
 botonGuardar.addEventListener("click",()=>{
     conjuntoSeleccionado.agregarALocarStorage();
-    contenedorCargarGuardar.appendChild(mensajePreguntasGuardadas);
-    setTimeout(()=> mensajePreguntasGuardadas.remove(),3000);
+    nuevoerror("preguntas guardadas :)",contenedorCargarGuardar,3000)
+    // contenedorCargarGuardar.appendChild(mensajePreguntasGuardadas);
+    // setTimeout(()=> mensajePreguntasGuardadas.remove(),3000);
 })
 botonEliminarTodo.addEventListener("click",()=>{
     primer.innerHTML="";
@@ -199,7 +207,6 @@ function crearDivDeConjunto(nuevoConjunto){
     conjuntosJuntos.appendChild(nombreDelConjuntoCreado);
     nombreDelConjuntoCreado.appendChild(contenedorNuevo);
     nuevoConjunto.lugar=contenedorNuevo;
-
     nuevoConjunto.crearDiv(nuevoConjunto.lugar);
 }
 function crearNuevoConjunto(nuevoConjunto){
@@ -210,9 +217,11 @@ const mensajeErrorSinNombre = document.createElement("p");
 function crearNuevoObjeto(nombreDelConjunto,preguntasDelArray,lugarDelArray) {
     let pasarConjunto = new conjuntoPreguntas(nombreDelConjunto,preguntasDelArray,lugarDelArray);
     todosLosConjuntos.push(pasarConjunto);
-
     crearNuevoConjunto(pasarConjunto);
 }
+// creo preguntas admin para que siempre se cargue
+botonUsarUsarPreguntasAdmin = document.querySelector(".boton-usar-preguntas-admin");
+botonUsarUsarPreguntasAdmin.addEventListener("click",function(){crearNuevoObjeto("preguntasAdmin",["Cuales son los roles interpersonales","Nombrar dos ejemplos de roles decisionales","Cuales son los tipos de habilides","Cuales son los niveles gerenciales organizacionales","Nombrar a quienes se los denomina gerentes de nivel institucional","Nombrar las variantes q estudia el entorno o ambiente global","Nombrar las formas de internacionalizar a una organización","Habilidad q predomina en el nivel operativo :","Competencias del administrador","Proceso del administrador defini cada uno","Nombra q hacen los diferentes generentes de los niveles organizacionales","Como se evalua el desempeño de un administrador","Características de una organizacion","Diferencias de objetivos organizacionales y objetivos individuales","Quales son los paremetros del sistema nombralos y definilos","Que es homeostasis","Tipos de retrolimentacion","Cuantas propiedades del sistema hay definilas","Como se clasifican los sistemas segun su constitucion y segun su naturaleza","Clasifica la org segun su finalidad, tamaño, regimen juridico, actividad economica","Como se clasifica la org por los sectores de actividad nombrarlos y definirlos","El ambiente general o contexto mediato impacta en la org de manera directa?(V/F)","Cuales son las variables q componen el entorno mediato o contexto general, ambiente de tarea y el ambiente interno","Aspectos formales e informales q son","Cuales son los aspectos de la cultura","Variables q se analizan dentro del riesgo politico","Cuales son las medidas politicas","Que significa outsourcing"],primer)});
 function validarNombreRepetido(nombreAValidar){
     //recorro los nombres de todos los conjuntos para validar qe no se repitan
     for(i=0;i<todosLosConjuntos.length;i++){
@@ -226,15 +235,17 @@ function validarNombreRepetido(nombreAValidar){
 botonCrearConjunto.addEventListener("click", ()=>{
     if(nombreDelConjunto===""||nombreDelConjunto===" "){
         //valido el nombre
-        mensajeErrorSinNombre.innerText = "debes ponerle un nombre valido a tu conjunto";
-        crearYNombrarConjunto.appendChild(mensajeErrorSinNombre);//! ! ! hacer en funcion
-        setTimeout(()=> mensajeErrorSinNombre.remove(),3000);
+        nuevoerror("debes ponerle un nombre valido a tu conjunto",crearYNombrarConjunto,3000)
+        // mensajeErrorSinNombre.innerText = "debes ponerle un nombre valido a tu conjunto";
+        // crearYNombrarConjunto.appendChild(mensajeErrorSinNombre);//! ! ! hacer en funcion
+        // setTimeout(()=> mensajeErrorSinNombre.remove(),3000);
     } else if(validarNombreRepetido(nombreDelConjunto)) {
         crearNuevoObjeto(nombreDelConjunto,[],)
     } else{
-        mensajeErrorSinNombre.innerText = `ya existe un conjunto llamado ${nombreDelConjunto}`;
-        crearYNombrarConjunto.appendChild(mensajeErrorSinNombre);
-        setTimeout(()=> mensajeErrorSinNombre.remove(),3000);
+        nuevoerror(`ya existe un conjunto llamado ${nombreDelConjunto}`,crearYNombrarConjunto,3000)
+        // mensajeErrorSinNombre.innerText = `ya existe un conjunto llamado ${nombreDelConjunto}`;
+        // crearYNombrarConjunto.appendChild(mensajeErrorSinNombre);
+        // setTimeout(()=> mensajeErrorSinNombre.remove(),3000);
     }
 })
 const nombreCreado = document.createElement("p");
@@ -244,20 +255,23 @@ botonAgregarNombre.addEventListener("click", ()=>{
     nombreDelConjunto = inputNombreConjunto.value;
     if(nombreDelConjunto==""||nombreDelConjunto==" "){
         //valido el nombre
-        nombreCreado.innerText ="nombre no valido";
-        contenedorNombres.appendChild(nombreCreado);
-        setTimeout(()=> nombreCreado.remove(),3000);
+        nuevoerror("nombre no valido",contenedorNombres,3000)
+        // nombreCreado.innerText ="nombre no valido";
+        // contenedorNombres.appendChild(nombreCreado);
+        // setTimeout(()=> nombreCreado.remove(),3000);
     }else {
-        nombreDelConjunto = inputNombreConjunto.value;
-        nombreCreado.innerText = "nombre elegido: "+ nombreDelConjunto;
-        contenedorNombres.appendChild(nombreCreado);
+        contenedorNombres.lastChild.remove();
+        mensajeDelNombreDelConjunto = "nombre elegido: "+ inputNombreConjunto.value;
+        nuevoerror(mensajeDelNombreDelConjunto,contenedorNombres,)
+        // contenedorNombres.appendChild(nombreCreado);
     }
     inputNombreConjunto.value = ""
 })
 
 botonGuardarConjuntos.addEventListener("click", ()=>{
-    mensajeConjuntosGuardados.innerText = "conjuntos guardados";
-    contenedorGuardarConjuntos.appendChild(mensajeConjuntosGuardados);
+    nuevoerror("conjuntos guardados",contenedorGuardarConjuntos,)
+    // mensajeConjuntosGuardados.innerText = "conjuntos guardados";
+    // contenedorGuardarConjuntos.appendChild(mensajeConjuntosGuardados);
     localStorage.setItem("conjuntosGuardados",JSON.stringify(todosLosConjuntos));
 })
 function cargarConjuntosGuardados(){
@@ -271,8 +285,11 @@ function cargarConjuntosGuardados(){
 }
 cargarConjuntosGuardados()
 botonEliminarConjuntosGuardados.addEventListener("click", ()=>{
-    mensajeConjuntosGuardados.innerText ="conjuntos borrados";
-    contenedorGuardarConjuntos.appendChild(mensajeConjuntosGuardados);
+    nuevoerror("conjuntos borrados",contenedorGuardarConjuntos,)
+    // mensajeConjuntosGuardados.innerText ="conjuntos borrados";
+    // contenedorGuardarConjuntos.appendChild(mensajeConjuntosGuardados);
     todosLosConjuntos=[]
     localStorage.setItem("conjuntosGuardados",JSON.stringify(todosLosConjuntos));
+    // ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
 })
+
